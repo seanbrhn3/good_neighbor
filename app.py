b7ae7a3c7588a0e611541cb833cc123923501a8f
sbrown31 <sbrown31@umd.edu>
2019-05-05 01:26:22 -0400

    adding everything

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
-
+import pymongo
+from authorize import Auth
 app = Flask(__name__)
-
+client = pymongo.MongoClient("mongodb+srv://seanbrhn3:45305006@goodneighbordb-1pr3o.mongodb.net/test?retryWrites=true")
+db = client.test
+trans = Auth()
+# trans.deleteAll()
+# trans.deletePool()
+# db.votes.remove()
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=="POST":
@@ -13,11 +19,70 @@ def index():
 
 @app.route("/register",methods=["GET","POST"])
 def register():
+    if request.method == "POST":
+        print("LOOK HERE",request.form)
+        trans.creds(request.form['account_number'],request.form['exp_date'],"100.00")
+        db.userInfo.insert_one({
+            "businessName": request.form['business_name'],
+            'account_number': request.form['account_number'],
+            'exp_date': request.form['exp_date'],
+            'description': request.form['description'],
+            'password': request.form['password'],
+            'amount_given': 100.00
+        })
+        if "pool" not in db.list_collection_names():
+            db.pool.insert_one({
+                "amount_given":100
+            })
+            return redirect(url_for("companies"))
+        else:
+            value = 0
+            for i in db.pool.find():
+                value = i['amount_given']
+            test = value+100
+            db.pool.insert_one(
+                {"amount_given":test}
+            )
+        return redirect(url_for("companies"))
     return render_template("register.html")
 
 @app.route("/signin",methods=["GET","POST"])
 def signin():
+    if request.method == "POST":
+        print(request.form)
+        foundUserAndPassword = False
+        for i in db.userInfo.find():
+            if i["businessName"] == request.form['business_name'] and i['password'] == request.form['password']:
+                return redirect(url_for("relief",company=request.form['business_name']))
     return render_template("signin.html")
+
+@app.route('/companies')
+def companies():
+    return render_template("companies.html",comps=db.userInfo.find(),pool=trans.pool())
+
+@app.route('/relief/<company>',methods=["GET","POST"])
+def relief(company):
+    company1 = db.userInfo.find({"businessName":company})
+    if request.method == "POST":
+        print(request.form.keys())
+        values = {}
+        values[company] = company
+        reasons = []
+        for i in request.form.keys():
+            reasons.append(request.form[i])
+        values['reasons'] = reasons
+        values["votes"] = 0
+        db.vote.insert_one(values)
+        return redirect(url_for('votes',comp=company))
+    return render_template("relief.html",company=company1)
+
+@app.route('/form/<request>',methods=["GET","POST"])
+def form(request):
+    return render_template("form.html",request=request)
+
+@app.route('/votes/<comp>',methods=["GET","POST"])
+def votes(comp):
+    return render_template("votes.html",values=db.vote.find())
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ authorize.py
@@ -0,0 +1,45 @@
+from authorizenet import apicontractsv1
+from authorizenet.apicontrollers import*
+from decimal import*
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://seanbrhn3:45305006@goodneighbordb-1pr3o.mongodb.net/test?retryWrites=true")
+db = client.test
+class Auth():
+    merchantAuth = apicontractsv1.merchantAuthenticationType()
+    merchantAuth.name ='29mBg5SW'
+    merchantAuth.transactionKey ='3F6xd6DY7E8gP44E'
+
+    def creds(self, cardNumber,exp_date,amount_to_take):
+        creditCard = apicontractsv1.creditCardType()
+        creditCard.cardNumber =cardNumber
+        creditCard.expirationDate =exp_date
+
+        payment = apicontractsv1.paymentType()
+        payment.creditCard = creditCard
+
+        transactionrequest = apicontractsv1.transactionRequestType()
+        transactionrequest.transactionType ="authCaptureTransaction"
+        transactionrequest.amount = Decimal (amount_to_take)
+        transactionrequest.payment = payment
+
+
+        createtransactionrequest = apicontractsv1.createTransactionRequest()
+        createtransactionrequest.merchantAuthentication = self.merchantAuth
+        createtransactionrequest.refId ="MerchantID-0001"
+
+        createtransactionrequest.transactionRequest = transactionrequest
+        createtransactioncontroller = createTransactionController(createtransactionrequest)
+        createtransactioncontroller.execute()
+
+        response = createtransactioncontroller.getresponse()
+
+        if (response.messages.resultCode=="Ok"):
+               print("Transaction ID : %s"% response.transactionResponse.transId)
+        else:
+               print("response code: %s"% response.messages.resultCode)
+    def deleteAll(self):
+        return db.userInfo.remove()
+    def deletePool(self):
+        return db.pool.remove()
+    def pool(self):
+        return db.pool.find()
